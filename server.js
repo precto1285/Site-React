5f543a509e2a339f5008a91077ea900fd9446a86
Philippe Recto <precto1285@gmal.com>
2019-05-16 22:04:02 -0700

    cleared react app and converted function component to class component

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const PORT = 5000;
 
 app.use(cors());
 
@@ -38,7 +38,5 @@ app.get('/customers', (req, res) => {
     }
   ]);
 });
-
-const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Connected to Port# ${PORT}`))
