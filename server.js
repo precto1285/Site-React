e8a1fc724e6d99f086a32175a3fe2880e1d196c7
Philippe Recto <precto1285@gmal.com>
2019-05-16 21:27:52 -0700

    added to server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Connected to Port# ${PORT}`))
