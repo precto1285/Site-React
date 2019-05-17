fcb85e3701163d2e03e10a420b90d437624c2f7a
Philippe Recto <precto1285@gmal.com>
2019-05-16 22:28:07 -0700

    added buttons

--- site-react/src/App.js
+++ site-react/src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="mt-5">
+      <div className="App container">
+        <div className="navbar text-center py-3 bg-primary text-white">
           <h1>Hello World!</h1>
+        </div>
+        <div className="jumbotron py-5 text-center"><h1>Hello World!</h1></div>
+        <div className="row">
+          <div className="col-md-3 my-1 p-2 shadow-lg bg-transparent"><button className="btn btn-danger text-white font-weight-bolder">See More</button></div>
+          <div className="col-md-3 my-1 p-2 shadow-lg bg-transparent"><button className="btn btn-danger text-white font-weight-bolder">See More</button></div>
+          <div className="col-md-3 my-1 p-2 shadow-lg bg-transparent"><button className="btn btn-danger text-white font-weight-bolder">See More</button></div>
+          <div className="col-md-3 my-1 p-2 shadow-lg bg-transparent"><button className="btn btn-danger text-white font-weight-bolder">See More</button></div>
         </div>
       </div>
     );
