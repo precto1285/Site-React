69f54c8cf20a109edc361ee84e72f00e5b2b4b90
Philippe Recto <precto1285@gmal.com>
2019-05-16 21:55:18 -0700

    added json to server and show on localhost:5000/customers

--- server.js
+++ server.js
@@ -5,6 +5,40 @@ const app = express();
 
 app.use(cors());
 
+app.get('/customers', (req, res) => {
+  console.log('Customers');
+  res.send(customers = [
+    {
+      firstName: "Ronald",
+      lastName: "Recto",
+      userName: "rvrecto",
+      email: "rvr@recto.com",
+      status: "active"
+    },
+    {
+      firstName: "Frank",
+      lastName: "Recto",
+      userName: "fpr2",
+      email: "frecto@recto.com",
+      status: "away"
+    },
+    {
+      firstName: "Phil",
+      lastName: "Recto",
+      userName: "prexwebs",
+      email: "prexswebs@gmail.com",
+      status: "disabled"
+    },
+    {
+      firstName: "Marty",
+      lastName: "Recto",
+      userName: "MDM4life",
+      email: "mdrecto@recto.com",
+      status: "active"
+    }
+  ]);
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Connected to Port# ${PORT}`))
