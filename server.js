a202e555b3443792d5e9ed5ee3de69fcfa15f586
Philippe Recto <precto1285@gmal.com>
2019-05-17 17:37:24 -0700

    connected and mapped data from server to component

--- server.js
+++ server.js
@@ -8,34 +8,40 @@ app.use(cors());
 app.get('/customers', (req, res) => {
   console.log('Customers');
   res.send(
-    {
-      firstName: "Ronald",
-      lastName: "Recto",
-      userName: "rvrecto",
-      email: "rvr@recto.com",
-      status: "active"
-    },
-    {
-      firstName: "Frank",
-      lastName: "Recto",
-      userName: "fpr2",
-      email: "frecto@recto.com",
-      status: "away"
-    },
-    {
-      firstName: "Phil",
-      lastName: "Recto",
-      userName: "prexwebs",
-      email: "prexswebs@gmail.com",
-      status: "disabled"
-    },
-    {
-      firstName: "Marty",
-      lastName: "Recto",
-      userName: "MDM4life",
-      email: "mdrecto@recto.com",
-      status: "active"
-    }
+    customers = [
+      {
+        key: 1,
+        firstName: "Ronald",
+        lastName: "Recto",
+        userName: "rvrecto",
+        email: "rvr@recto.com",
+        status: "active"
+      },
+      {
+        key: 2,
+        firstName: "Frank",
+        lastName: "Recto",
+        userName: "fpr2",
+        email: "frecto@recto.com",
+        status: "away"
+      },
+      {
+        key: 3,
+        firstName: "Phil",
+        lastName: "Recto",
+        userName: "prexwebs",
+        email: "prexswebs@gmail.com",
+        status: "disabled"
+      },
+      {
+        key: 4,
+        firstName: "Marty",
+        lastName: "Recto",
+        userName: "MDM4life",
+        email: "mdrecto@recto.com",
+        status: "active"
+      }
+    ]
   );
 });
 
--- site-react/src/App.js
+++ site-react/src/App.js
@@ -9,11 +9,12 @@ class App extends Component {
     }
   }
 
-  componentDidMount(req, res) {
+  componentDidMount() {
     let url = "http://localhost:5000/customers";
     fetch(url)
       .then(res => { return res.json() })
-      .then(data => { this.setState({ customers: data }) })
+      .then(customers => { this.setState({ customers: customers }) })
+      .catch(error => console.log(error));
   }
 
   render() {
@@ -24,42 +25,22 @@ class App extends Component {
         </div>
         <div className="jumbotron py-5 text-center"><h1>Hello World!</h1></div>
         <div className="row">
-          <div className="col-sm-3">
-            <InfoCard
-              firstName={this.state.customers.firstName}
-              lastName={this.state.customers.lastName}
-              userName={this.state.customers.firstName}
-              email={this.state.customers.email}
-              status={this.state.customers.status}
-            />
-          </div>
-          <div className="col-sm-3">
-            <InfoCard
-              firstName={this.state.customers.firstName}
-              lastName={this.state.customers.lastName}
-              userName={this.state.customers.firstName}
-              email={this.state.customers.email}
-              status={this.state.customers.status}
-            />
-          </div>
-          <div className="col-sm-3">
-            <InfoCard
-              firstName={this.state.customers.firstName}
-              lastName={this.state.customers.lastName}
-              userName={this.state.customers.firstName}
-              email={this.state.customers.email}
-              status={this.state.customers.status}
-            />
-          </div>
-          <div className="col-sm-3">
-            <InfoCard
-              firstName={this.state.customers.firstName}
-              lastName={this.state.customers.lastName}
-              userName={this.state.customers.firstName}
-              email={this.state.customers.email}
-              status={this.state.customers.status}
-            />
-          </div>
+          {
+            this.state.customers.map((customer) => {
+              return <div className="col-sm-3">
+                <InfoCard
+                  key={customer.key}
+                  firstName={customer.firstName}
+                  lastName={customer.lastName}
+                  userName={customer.firstName}
+                  email={customer.email}
+                  status={customer.status}
+                />
+              </div>
+            })
+
+
+          }
         </div>
       </div>
     );
